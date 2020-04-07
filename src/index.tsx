1252f4a35ed76e0c363a00c76e857985fa15793a
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-04-07 15:45:19 +0200

    style with styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from 'styled-components'
 import { useCallback, useState, useReducer } from "react";
 import { filter, map, get, curry, clone, concat } from "lodash";
 import { render } from "react-dom";
@@ -25,6 +26,11 @@ type RouteMap = {
     dispatch: (action: Action) => void;
   }>;
 };
+
+function messageCountString(messageCount: number): string {
+  return messageCount <= 5 ? "" + messageCount : "5+"
+}
+
 
 interface State {
   route: Route;
@@ -76,40 +82,56 @@ function App({
 
   return (
     <main>
-      <Nav messageCount={state.unreadMessages} dispatch={dispatch} />
+      <StyledNav messageCount={state.unreadMessages} dispatch={dispatch} />
       <Route state={state} dispatch={dispatch} />
     </main>
   );
 }
 
+const NavLink = styled.a`
+  color: ivory;
+  cursor: pointer;
+  text-decoration: none;
+  margin-right: 20px;
+`
+
 function Nav({
+  className,
   messageCount,
   dispatch,
 }: React.PropsWithChildren<{
+  className: string;
   messageCount: number;
   dispatch: (action: Action) => any;
 }>): React.ReactElement | null {
   return (
-    <nav>
-      <a
+    <nav className={className}>
+      <NavLink
         id="messages-link"
         onClick={() =>
           dispatch({ type: ActionType.CHANGE_ROUTE, data: Route.MESSAGES })
         }
       >
-        Messages (<span id="messages-count">{messageCount}</span> new)
-      </a>
-      <a
+        Messages (<span id="messages-count">{messageCountString(messageCount)}</span> new)
+      </NavLink>
+      <NavLink
         id="new-message-link"
         onClick={() =>
           dispatch({ type: ActionType.CHANGE_ROUTE, data: Route.FORM })
         }
       >
         Add new message
-      </a>
+      </NavLink>
     </nav>
   );
 }
+
+const StyledNav = styled(Nav)`
+  background-color: steelblue;
+  height: 50px;
+  line-height: 50px;
+  text-align: right;
+`
 
 interface Message {
   id: number;
@@ -119,15 +141,17 @@ interface Message {
 }
 
 function Message({
+  className,
   message,
   dispatch,
 }: React.PropsWithChildren<{
+  className: string;
   message: Message;
   dispatch: (action: Action) => void;
 }>): JSX.Element {
   return (
     <article
-      className={!message.read ? "unread" : ""}
+      className={className}
       onClick={(): void => 
         !message.read && dispatch({ type: ActionType.MARK_MESSAGE_READ, data: message })
       }
@@ -138,36 +162,93 @@ function Message({
   );
 }
 
+const StyledMessage = styled(Message)`
+  border-bottom: 1px solid dodgerblue;
+  padding: 20px;
+  background-color: ${p => {
+    console.log(p) 
+    return !p.message.read && "lightcyan"}};
+`
+function MessageNotificationBanner({className, unreadMessages}: React.PropsWithChildren<{
+  className: string;
+  unreadMessages: number;
+}>): React.ReactElement {        
+  return (
+    <p className={className}>
+      You have
+      <span>
+        {` ${messageCountString(unreadMessages)} `}
+      </span>
+      new messages!
+    </p>
+  )
+}
+
+const StyledMessageNotificationBanner = styled(MessageNotificationBanner)`
+  color: red;
+  padding: 20px;
+`
+
 function Messages({
+  className,
   state,
   dispatch,
 }: React.PropsWithChildren<{
+  className: string;
   state: State;
   dispatch: (action: Action) => void;
 }>): React.ReactElement | null {
   const { messages, unreadMessages } = state;
   console.log(messages);
   return (
-    <section id="messages-section">
+    <section className={className}>
       {unreadMessages > 0 && (
-        <p id="messages-notification">
-          You have
-          <span id="messages-notification-count">{` ${unreadMessages} `}</span>{" "}
-          new messages!
-        </p>
+        <StyledMessageNotificationBanner unreadMessages={unreadMessages} />
       )}
       {map(messages, (message) => {
         console.log(message);
-        return <Message key={message.id} message={message} dispatch={dispatch}/>;
+        return <StyledMessage key={message.id} message={message} dispatch={dispatch}/>;
       })}
     </section>
   );
 }
 
+const StyledMessages = styled(Messages)`
+article:first-of-type {
+  border-top: 1px solid dodgerblue;
+}
+`
+
+const FormButton = styled.button`
+  background-color: steelblue;
+  border: none;
+  border-radius: 3px;
+  color: ivory;
+  padding: 5px;
+`
+
+const StyledInput = styled.input`
+  display: block;
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 5px;
+  border: 1px solid dodgerblue;
+`
+
+const StyledTextarea = styled.textarea`
+  display: block;
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 5px;
+  border: 1px solid dodgerblue;
+`
+
 function NewMessageForm({
+  className,
   state,
   dispatch,
 }: React.PropsWithChildren<{
+  className: string;
   state: State;
   dispatch: (action: Action) => void;
 }>): React.ReactElement | null {
@@ -182,7 +263,7 @@ function NewMessageForm({
   );
 
   return (
-    <section id="form-section">
+    <section className={className}>
       <form
         id="form"
         onSubmit={(e: FormEvent<HTMLFormElement>) => {
@@ -194,7 +275,7 @@ function NewMessageForm({
         }}
       >
         <label htmlFor="subject">Subject</label>
-        <input
+        <StyledInput
           required
           type="text"
           name="subject"
@@ -203,21 +284,24 @@ function NewMessageForm({
         />
 
         <label htmlFor="body">Body</label>
-        <textarea
+        <StyledTextarea
           required
           name="body"
           onChange={saveValue("body")}
           value={get(formState, "body") || ""}
-        ></textarea>
-        <button>Submit!</button>
+        ></StyledTextarea>
+        <FormButton>Submit!</FormButton>
       </form>
     </section>
   );
 }
 
+const StyledNewMessageForm = styled(NewMessageForm)`
+  padding: 20px;
+`
 const routes: RouteMap = {
-  [Route.FORM]: NewMessageForm,
-  [Route.MESSAGES]: Messages,
+  [Route.FORM]: StyledNewMessageForm,
+  [Route.MESSAGES]: StyledMessages,
 };
 
 render(<App routes={routes} />, document.getElementById("root"));
